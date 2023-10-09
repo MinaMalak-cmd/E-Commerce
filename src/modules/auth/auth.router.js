a9bb2a1ae6e13db4e1ff59ec3f77a6f02da1c07d
MinaMalak-cmd <engmina15@yahoo.com>
2023-10-09 13:35:22 +0300

    add user model

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -6,6 +6,12 @@ import { validation } from "../../../middlewares/validations/validation.js";
 const router = Router();
 
 router.post("/signup", validation(validators.signup), authController.signup);
-
+router.get("/users", authController.getAllUsers);
+router.patch("/change-password", validation(validators.changePassword), authController.changePassword);
+router.post("/login", validation(validators.login), authController.login);
+router.get("/confirm-email/:token", authController.confirmEmail);
+router.get("/new-confirm-email/:token", authController.newConfirmEmail);
+router.get("/unsubscribe/:token", authController.unSubscribe);
+router.post("/forget-password", validation(validators.forgetPassword), authController.forgetPassword);
 
 export default router;
