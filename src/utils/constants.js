bdcac535f965c439ca94e25b1928819d6c227198
MinaMalak-cmd <engmina15@yahoo.com>
2023-10-23 14:46:21 +0300

    create order model

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+
+export const paymentMethods = {
+    CASH: 'cash',
+    CARD: 'card',
+}
+
+export const orderStatus = {
+    PLACED: 'placed',
+    PENDING: 'pending',
+    CONFIRMED : 'confirmed',
+    ONWAY : 'onway',
+    DELIVERED: 'delivered',
+    CANCELLED: 'cancelled',
+}
