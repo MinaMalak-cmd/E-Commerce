ead96391c666b4774dd21e80f7357135c740ef8a
MinaMalak-cmd <engmina15@yahoo.com>
2023-10-28 11:51:05 +0200

    handlle permissions

--- src/modules/carts/cart.router.js
+++ src/modules/carts/cart.router.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import * as cartController from "./controllers/cart.controller.js";
 import { validation } from "../../../middlewares/validations/validation.js";
 import handleAuth from "../../../middlewares/handleAuth.js";
+import handleUserRole from "../../../middlewares/handleUserRole.js";
 import * as cartValidators from "../../../middlewares/validations/cart.validation.js";
+import { systemRoles } from "../../utils/constants.js";
 
 const router = Router();
 
 router.post('/', handleAuth, validation(cartValidators.addCart), cartController.addToCart);
-router.get('/',  cartController.getAllCarts);
+router.get('/',  handleAuth, handleUserRole([systemRoles.SUPER_ADMIN]), cartController.getAllCarts);
 router.delete('/:productId', handleAuth, cartController.deleteFromCart);
 router.delete('/', handleAuth, cartController.deleteAllCartsForThisUser);
 export default router;
