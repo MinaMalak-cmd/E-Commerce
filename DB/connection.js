de2f9c715d47436249f5f4b938660defd6e3675a
MinaMalak-cmd <engmina15@yahoo.com>
2023-11-14 15:51:56 +0200

    change connection string

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 
 const connectDB = async () =>{
-    return await mongoose.connect(`${process.env.DB_URL}/E-Commerce`)
+    // return await mongoose.connect(process.env.DB_URL)
+    return await mongoose.connect(process.env.DB_URL_CLOUD)
     .then((res) =>{
         console.log("🚀DB Connected .........")
     }).catch((err) =>{
